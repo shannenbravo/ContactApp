81334b09e21ec9dc1d4686d6f8c278dd1376aac7
shannenbravo <shannenbravo@gmail.com>
2018-05-21 14:12:38 -0400

    added table view

---
+++ ContactsApp/UIColor+theme.swift
@@ -0,0 +1,16 @@
+//
+//  UIColor+theme.swift
+//  ContactsApp
+//
+//  Created by Shannen Bravo-Brown on 5/21/18.
+//  Copyright © 2018 Shannen Bravo-Brown. All rights reserved.
+//
+
+import UIKit
+extension UIColor{
+    static let pastelPink = UIColor(red: 244/255, green: 195/255, blue: 216/255, alpha: 1)
+    static let pastelBlue = UIColor(red: 124/255, green: 172/255, blue: 234/255, alpha: 1)
+    static let pastelGrey = UIColor(red: 207/255, green: 213/255, blue: 225/255, alpha: 1)
+    static let pastelYellow = UIColor(red: 251/255, green: 233/255, blue: 195/255, alpha: 1)
+}
+
--- ContactsApp/ViewController.swift
+++ ContactsApp/ViewController.swift
@@ -8,19 +8,50 @@
 
 import UIKit
 
-class ViewController: UIViewController {
+class ViewController: UITableViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
         setupNavBar()
-        view.backgroundColor = .purple
+//        view.backgroundColor = .white
+        tableView.backgroundColor = .white
+        tableView.register(UITableViewCell.self, forCellReuseIdentifier: "cellId")
+        tableView.tableFooterView = UIView();
     }
     
     func setupNavBar(){
         navigationItem.title = "Contacts"
+        navigationController?.navigationBar.barTintColor = UIColor.pastelGrey
         navigationController?.navigationBar.prefersLargeTitles = true;
+        navigationController?.navigationBar.titleTextAttributes = [NSAttributedStringKey.foregroundColor:UIColor.white]
         navigationController?.navigationBar.largeTitleTextAttributes = [NSAttributedStringKey.foregroundColor:UIColor.white]
+        navigationItem.rightBarButtonItem = UIBarButtonItem(image: #imageLiteral(resourceName: "addIcon 2").withRenderingMode(.alwaysOriginal), style: .plain, target: self, action: #selector(handleAddContact))
         
+    }
+    
+    @objc func handleAddContact (){
+        print("Adding Contact")
+    }
+    override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCell(withIdentifier: "cellId", for: indexPath)
+        cell.backgroundColor = .white
+        cell.textLabel?.text = "TESTING"
+        cell.textLabel?.textColor = .black
+        cell.textLabel?.font = UIFont.boldSystemFont(ofSize: 18)
+        return cell
+    }
+    
+    override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return 10
+    }
+    override func tableView(_ tableView: UITableView, viewForHeaderInSection section: Int) -> UIView? {
+        let view = UIView();
+        view.backgroundColor = .pastelYellow;
+        return view
+    }
+    
+    override func tableView(_ tableView: UITableView, heightForHeaderInSection section: Int) -> CGFloat {
+        return 50 
     }
 
 
