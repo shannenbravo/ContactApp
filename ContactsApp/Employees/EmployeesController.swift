ed03533855f71556f516ac95c049648065b1eec1
shannenbravo <shannenbravo@gmail.com>
2018-06-07 19:13:38 -0400

    added emplyee sections

--- ContactsApp/Employees/EmployeesController.swift
+++ ContactsApp/Employees/EmployeesController.swift
@@ -9,6 +9,14 @@
 import UIKit
 import CoreData
 
+class IndentLabel: UILabel {
+    override func drawText(in rect: CGRect) {
+        let insets = UIEdgeInsets(top: 0, left: 16, bottom: 0, right: 0)
+        let customRec = UIEdgeInsetsInsetRect(rect, insets)
+        super.drawText(in: customRec)
+    }
+}
+
 class EmployeesController: UITableViewController, CreateEmployeeControllerDelegate {
     func didAddEmployee(employee: Employee) {
         employees.append(employee)
@@ -33,9 +41,35 @@ class EmployeesController: UITableViewController, CreateEmployeeControllerDelega
         tableView.register(UITableViewCell.self, forCellReuseIdentifier: cellid)
     }
     
+    override func tableView(_ tableView: UITableView, viewForHeaderInSection section: Int) -> UIView? {
+        let label = IndentLabel()
+        if section == 0{
+            label.text = "Short Names"
+        }else if section == 1{
+            label.text = "Long Names"
+        }else{
+            label.text = "Really Long Names"
+        }
+        label.backgroundColor = UIColor.pastelBlue
+        label.font = UIFont.boldSystemFont(ofSize: 16)
+        return label
+    }
+    
+    override func tableView(_ tableView: UITableView, heightForHeaderInSection section: Int) -> CGFloat {
+        return 50
+    }
+    
+    override func numberOfSections(in tableView: UITableView) -> Int {
+        return allEmployees.count
+    }
+    
     override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
         let cell = tableView.dequeueReusableCell(withIdentifier: cellid, for: indexPath)
-        let employee = employees[indexPath.row]
+//        let employee = employees[indexPath.row]
+//        let employee = indexPath.section == 0 ? shortNameEmployee[indexPath.row] : longNameEmpolyee[indexPath.row]
+        let employee = allEmployees[indexPath.section][indexPath.row]
+        
+        
         cell.backgroundColor = UIColor.pastelYellow
         cell.textLabel?.text = employee.name
         if let birthday = employee.employeeInformation?.birthdate {
@@ -54,12 +88,44 @@ class EmployeesController: UITableViewController, CreateEmployeeControllerDelega
     }
     
     override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
-        return employees.count
+        return allEmployees[section].count
     }
+    
+    var shortNameEmployee = [Employee]()
+    var longNameEmpolyee = [Employee]()
+    var reallyLonhEmoloyees = [Employee]()
+    var allEmployees = [[Employee]]()
     
     func fetchData(){
         guard let companyEmployees = company?.employees?.allObjects as? [Employee] else {return}
-        self.employees = companyEmployees
+        shortNameEmployee = companyEmployees.filter({ (employee) -> Bool in
+            if let count = employee.name?.count{
+                return count < 6
+            }
+            return false
+        })
+        
+        longNameEmpolyee = companyEmployees.filter({ (employee) -> Bool in
+            if let count = employee.name?.count{
+                return count > 6 && count < 9
+            }
+            return false
+        })
+        
+        reallyLonhEmoloyees = companyEmployees.filter({ (employee) -> Bool in
+            if let count = employee.name?.count{
+                return count > 9
+            }
+            
+            return false
+        })
+//        self.employees = companyEmployees
+        
+        allEmployees = [
+            shortNameEmployee,
+            longNameEmpolyee,
+            reallyLonhEmoloyees
+        ]
         
     }
     
